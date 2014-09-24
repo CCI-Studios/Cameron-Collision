6c6dcfdc737667bb5bd964edd9fae776b06e7e92
Chris Morris <cmorris@ccistudios.com>
2014-09-24 08:53:31 -0400

    Added live alias for drush

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -11,3 +11,12 @@ $aliases['dev'] = array(
 	'ssh-options'=>'-p 37241'
 );
 
+$aliases['live'] = array(
+	'uri'=> 'cameroncollision.com',
+	'root' => '/home/cameron/subdomains/live/public_html',
+	'remote-host'=> 'host.cciserver2.com',
+	'remote-user'=> 'cameron',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
